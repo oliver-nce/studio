1de752b960c3f2d5a28b2b61e2834329e8a73ed1
Oliver Reid <ocreid@users.noreply.github.com>
2026-03-25 18:49:26 -0400

    fix: escape curly braces in PathFinderPanel title attr (vite build error)

--- frontend/src/nce/components/PathFinder/PathFinderPanel.vue
+++ frontend/src/nce/components/PathFinder/PathFinderPanel.vue
@@ -88,7 +88,7 @@
 					size="sm"
 					@click="insertAsExpression"
 					:disabled="!canInsertBinding"
-					title="Insert as {{ expression }}"
+					title="Insert as &lcub;&lcub; expression &rcub;&rcub;"
 				>
 					{{ '{{ }}' }}
 				</Button>
